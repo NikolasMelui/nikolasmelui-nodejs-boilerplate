36107a36a89d26f8d0acc9eeb0f6a27daf7486eb
NikolasMelui <slak@samaradom.ru>
2018-10-23 10:18:16 +0400

    Add jest env to the .eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
+    jest: true,
   },
   extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
-    sourceType: 'module'
+    sourceType: 'module',
   },
   rules: {
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
     semi: ['error', 'always'],
-    'no-console': ['allow', 'always'],
-    'linebreak-style': ['error', 'unix']
-  }
+    'no-console': 0,
+    'linebreak-style': ['error', 'unix'],
+  },
 };
