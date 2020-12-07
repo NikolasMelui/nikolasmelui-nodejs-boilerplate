23de794983486d9779c746277d1d3154670e1a4d
nsladkii <nsladkii@voximplant.com>
2020-12-07 12:53:30 +0300

    Move http server functions to server.js file

--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
 'use strict';
 
 const http = require('http');
-const { APPLICATION_HOST, APPLICATION_PORT } = require('./config');
 const { getApplicationHost, getApplicationPort } = require('./helpers');
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
-    res.end(
-      `Hello from ${getApplicationHost(req)}:${getApplicationPort(
-        req,
-      )} and welcome to the nikolasmelui-node-boilerplate!`,
+const server = (host, port) => {
+  http
+    .createServer((req, res) => {
+      res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+      res.end(
+        `Hello from ${getApplicationHost(req)}:${getApplicationPort(
+          req,
+        )} and welcome to the nikolasmelui-node-boilerplate!`,
+      );
+    })
+    .listen(port, host, () =>
+      console.info(`Server is listening on http://${host}:${port}`),
     );
-  })
-  .listen(APPLICATION_PORT, APPLICATION_HOST, () =>
-    console.log(
-      `Server is listening on ${APPLICATION_HOST}:${APPLICATION_PORT}`,
-    ),
-  );
+};
+
+module.exports = server;
