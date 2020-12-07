fb6fe1afd86b4e7afe979ba09de68a78b4685be0
nsladkii <nsladkii@voximplant.com>
2020-12-07 14:49:05 +0300

    Add process.env variables instead of config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const server = require('./server');
 
 const { APPLICATION_HOST, APPLICATION_PORT } = require('./config');
 
+const host = process.env.APPLICATION_HOST || APPLICATION_HOST;
+const port = process.env.APPLICATION_PORT || APPLICATION_PORT;
+
 (async () => {
-  await server(APPLICATION_HOST, APPLICATION_PORT);
+  await server(host, port);
 })();
